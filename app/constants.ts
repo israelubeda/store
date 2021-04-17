77bc99bec4338b49d093b5799694a7d194fff6ec
diegobollini <diego.bollini@protonmail.com>
2021-04-17 19:08:53 -0300

    test planilla

--- app/constants.ts
+++ app/constants.ts
@@ -1,19 +1,19 @@
 export const INFORMATION = {
   avatar: "/assets/avatar.jpg",
   banner: "/assets/banner.jpg",
-  title: "URBN Catering",
-  description: "Galardonada pizza estilo New Haven en Palermo SOHO.",
-  phone: `5491141414141`,
-  sheet: `https://docs.google.com/spreadsheets/d/e/2PACX-1vQG1pOSbGxaJYzlOWF2t7KFyP5wtWo_EDS0crTllQtALvDDZBq7OwxvTDJwM4dmasW7wmNh6KFB443j/pub?output=csv`,
+  title: "La Libre",
+  description: "Cultura | Feminismo | Revolución.",
+  phone: `543413879932`,
+  sheet: `https://docs.google.com/spreadsheets/d/e/2PACX-1vS8wxp9GvOkeIz8LUsOpIbngFliMgsQd0ZnWEiqkrInhNfDY6E8cNyyKyJCNIuQTBD4U2St9g3VE4Pw/pub?gid=0&single=true&output=csv`,
   color: `teal`,
   social: [
     {
       name: "instagram",
-      url: `https://instagram.com/goncy.js`,
+      url: `https://www.instagram.com/la__libre/`,
     },
     {
       name: "whatsapp",
-      url: `https://wa.me/5491141414141`,
+      url: `https://api.whatsapp.com/send?phone=543413879932&text=%F0%9F%98%80%20Hola%20La%20Libre!%20Puedo%20hacerles%20una%20consulta%3F%0AMuchas%20gracias%20%F0%9F%92%9C`,
     },
   ],
 };
